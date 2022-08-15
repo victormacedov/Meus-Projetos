a756d8b8ef66d8274faa74eb0454a97153b9fc45
Victor Macedo <victormacedo1001@gmail.com>
2022-08-15 01:29:13 -0300

    commit inicial

---
+++ Jogo - Par Ou Impar.py
@@ -0,0 +1,33 @@
+import random
+
+contvitorias = 0
+print('=' * 40)
+print('Jogo - Par ou Impar')
+while True:
+    print('=' * 40)
+    n = int(input('Digite o seu número: '))
+    print('=' * 40)
+    escolhacomputador = random.randint(1, 10)
+    soma = n + escolhacomputador
+    tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {n} e o computador {escolhacomputador} -> Total: {soma}')
+    if tipo == 'P':
+        if soma % 2 == 0:
+            print(f'Você venceu!')
+            contvitorias += 1
+        else:
+            print('Você perdeu!')
+            pergunta = (input('Você deseja continuar jogando? [S/N]')).strip().upper()[0]
+            if pergunta == "N":
+                break
+    if tipo == 'I':
+        if soma % 2 == 1:
+            print('Você venceu!')
+            contvitorias += 1
+        else:
+            print('Você perdeu!')
+            pergunta = (input('Você deseja continuar jogando? [S/N]')).strip().upper()[0]
+            if pergunta == "N":
+                break
+        print('vamos jogar novamente...')
+print(f'O jogo acabou! Você venceu {contvitorias} vezes')
